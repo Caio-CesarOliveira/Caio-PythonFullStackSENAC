8afc1f0f3b8f5a0ddac498d1a43a61fb6f07927a
Caio-CesarOliveira <caiocs919@outlook.com>
2025-08-15 20:35:20 -0300

    calculadora.js

--- calculadora.js
+++ calculadora.js
@@ -1,21 +1,10 @@
-<html>
-<head>
-    <title>Calculadora Simples</title>
-</head>
-<body>
-<script>
-    let x = prompt("Escolha a operação: (s) soma, (su) subtração, (m) multiplicação, (d) divisão");
-    let n1 = parseFloat(prompt("Entre com o primeiro número"));
-    let n2 = parseFloat(prompt("Entre com o segundo número"));
-    let resultado;
-
-    if (x == "s") resultado = n1 + n2;
-    else if (x == "su") resultado = n1 - n2;
-    else if (x == "m") resultado = n1 * n2;
-    else if (x == "d") resultado = n1 / n2;
-    else resultado = "Operação inválida";
-
-    alert("O resultado é: " + resultado);
-</script>
-</body>
-</html>
+let num1 = 10;
+let num2 = 5;
+let soma = num1 + num2;
+let subtracao = num1 - num2;
+let multiplicacao = num1 * num2;
+let divisao = num1 / num2;
+console.log("A soma é: ", soma);
+console.log("A subtração é: ", subtracao);  
+console.log("A multiplicação é: ", multiplicacao);
+console.log("A divisão é: ", divisao);
